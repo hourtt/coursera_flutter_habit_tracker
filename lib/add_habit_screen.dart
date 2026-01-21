9b4e150b1ae6afb01a50ef1a4904bb258aedd3de
hourtt <bunhourtjesda@gmail.com>
2026-01-21 21:13:50 +0700

    Set default selected color name for add habit screen

--- lib/add_habit_screen.dart
+++ lib/add_habit_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
 class AddHabitScreen extends StatefulWidget {
@@ -22,7 +24,7 @@ class _AddHabitScreenState extends State<AddHabitScreen> {
     'Teal': Colors.teal,
     'Deep Purple': Colors.deepPurple,
   };
-  String selectedColorName = 'Amber'; // Default color name
+  String selectedColorName = 'Amber'; //* Set default selected color name
 
   @override
   void initState() {
